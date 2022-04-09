396a78bb2824471b432d57bfbf8817616591d8dd
Lynnea <lruan.dev@gmail.com>
2022-04-09 11:49:48 -0400

    Task 3- Added comments to CampsiteInfo to render beside card Img

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -18,6 +18,29 @@ class CampsiteInfo extends Component {
     )
   }
   
+  //Task 3: A description will appear on the right side of the description card
+  renderComments(comments) {
+    if(comments) {
+      return (
+        <div className='col-md-5 m-1'>
+          <h4>Comments</h4>
+          {comments.map(comment => <div key={comment.id}>{comment.text}<br />
+          -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+            <div>
+              {<br />}
+            </div>
+          </div>)}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+
+        </div>
+      );
+    }
+  }
+
   //Task 1: Checking to see if campsite is evaluated as truthy/falsy (logic)
   //If statement will return empty <div> w/bootrap row if truthy and falsy will return empty <div>
 
@@ -27,6 +50,7 @@ class CampsiteInfo extends Component {
       return (
         <div className='row'>
           {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
         </div>
       );
     } else {
