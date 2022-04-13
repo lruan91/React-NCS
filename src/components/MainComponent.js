2392c8754dbb7b9841e9e61a07ce0383d29691b1
Lynnea <lruan.dev@gmail.com>
2022-04-13 00:36:36 -0400

    Removed onClick even handler

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
-      selectedCampsite: null
     };
   }
 
-  onCampsiteSelect(campsiteId) {
-    this.setState({selectedCampsite: campsiteId});
-  }
 
   render() {
     return (
