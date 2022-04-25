6b7ba98611484df5d6c19bba26401bfd12495c1e
Lynnea <lruan.dev@gmail.com>
2022-04-25 13:29:33 -0400

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
   Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
   //Week 4 Task 3: Validation
   // Making sure the length is no less than 2, or greater than 15 letters
@@ -52,6 +53,26 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
   //Task 2: Calling renderCampsite: The campsite will appear below the 4 cards with an img+description
   function CampsiteInfo(props) {
+    if(props.isLoading) {
+      return (
+        <div className='container'>
+          <div className='row'>
+            <Loading />
+          </div>
+        </div>
+      );
+    }
+    if(props.errMess) {
+      return (
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <h4>{props.errMess}</h4>
+            </div>
+          </div>
+        </div>
+      );
+    }
     if(props.campsite){
       return (
         <div className='container'>
