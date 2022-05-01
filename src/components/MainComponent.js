fd0ba6a8dd5b5694a5df151dafb65b7c0541b0fc
Lynnea <lruan.dev@gmail.com>
2022-04-30 22:47:25 -0400

    Week 5 Task 2: Update MainComponent: import postFeedback, add postFeedback to mapDispatch, pass postFeedback to the contact component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions, fetchPartners, postFeedback } from '../redux/ActionCreators';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
@@ -22,21 +22,26 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-  postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
-    fetchPromotions: () => (fetchPromotions())
+    fetchPromotions: () => (fetchPromotions()),
+    fetchPartners: () => (fetchPartners()),
+    postFeedback: (feedback) => (postFeedback(feedback))
 };
 
+//Locally Scoped -This component is being defined inside of the main component so it's only accessible inside the main component 
 class Main extends Component {
-  //Locally Scoped -This component is being defined inside of the main component so it's only accessible inside the main component 
+  
   componentDidMount() {
     this.props.fetchCampsites();
     this.props.fetchComments();
     this.props.fetchPromotions();
+    this.props.fetchPartners();
   }
 
+  //Week 5 Task 1: Update MainComponent: adding Partner isLoading, errMess and partner obj.
   render() {
     const HomePage = () => {
       return (
@@ -80,7 +85,7 @@ class Main extends Component {
               <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} 
               />} />
               <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-              <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+              <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
               <Route exact path='/aboutus' render = {() => <About partners={this.props.partners} />} />
               <Route exact path='/aboutus' component={About} />
               <Redirect to='/home' />
