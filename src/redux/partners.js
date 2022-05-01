544f6eb5c5a824395a86aecf4be144ba7c1bea21
Lynnea <lruan.dev@gmail.com>
2022-04-30 22:23:25 -0400

    Updated reducer in partners.js to respond to 3 actions

--- src/redux/partners.js
+++ src/redux/partners.js
@@ -1,7 +1,19 @@
-import { PARTNERS } from '../shared/partners';
-
-export const Partners = (state = PARTNERS, action) => {
+import * as ActionTypes from './ActionTypes';
+//Week 5 Task 1: Updated reducer code
+export const Partners = (state = {
+  
+    isLoading: true,
+    errMess: null,
+    partners: []
+  }, action) => {
+  
   switch (action.type) {
+    case ActionTypes.ADD_PARTNERS:
+    return {...state, isLoading: false, errMess: null, partners: action.payload};
+  case ActionTypes.PARTNERS_LOADING:
+    return {...state, isLoading: true, errMess: null, partners: []};
+  case ActionTypes.PARTNERS_FAILED:
+    return {...state, isLoading: false, errMess: action.payload};
     default: 
       return state;
   }
