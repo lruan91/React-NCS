199a2e001e8b6f64f132e7da21ad9c29484b0d6e
Lynnea <lruan.dev@gmail.com>
2022-04-30 22:26:12 -0400

    Week 5 Task 1: Update MainComponent - update Home component to pass isLoading, errMess and partners

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -47,7 +47,9 @@ class Main extends Component {
           promotion={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
           promotionLoading={this.props.promotions.isLoading}
           promotionErrMess={this.props.promotions.errMess}
-          partner={this.props.partners.filter(partner => partner.featured)[0]}
+          partner={this.props.partners.partners.filter(partner => partner.featured)[0]}
+          partnersLoading={this.props.partners.isLoading}
+          partnersErrMess={this.props.partners.errMess}
         />
       );
     }
